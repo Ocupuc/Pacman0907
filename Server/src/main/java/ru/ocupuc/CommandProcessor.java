15817d21850974f87e2acb8044e5a61df527cdd6
Ocupuc <108402919+Ocupuc@users.noreply.github.com>
2023-07-17 16:49:19 +0500

    пакман двигается)

--- Server/src/main/java/ru/ocupuc/CommandProcessor.java
+++ Server/src/main/java/ru/ocupuc/CommandProcessor.java
@@ -1,5 +1,8 @@
 package ru.ocupuc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandProcessor {
 
     private Field field;
@@ -12,19 +15,21 @@ public class CommandProcessor {
         Point currentPosition = field.getPacmanPosition();
         Point newPosition = null;
 
-        switch (command) {
-            case "W:true, S:false, A:false, D:false":
-                newPosition = new Point(currentPosition.x, currentPosition.y + 1);
-                break;
-            case "W:false, S:true, A:false, D:false":
-                newPosition = new Point(currentPosition.x, currentPosition.y - 1);
-                break;
-            case "W:false, S:false, A:true, D:false":
-                newPosition = new Point(currentPosition.x - 1, currentPosition.y);
-                break;
-            case "W:false, S:false, A:false, D:true":
-                newPosition = new Point(currentPosition.x + 1, currentPosition.y);
-                break;
+        String[] commands = command.split(", ");
+        Map<String, Boolean> commandMap = new HashMap<>();
+        for (String singleCommand : commands) {
+            String[] parts = singleCommand.split(":");
+            commandMap.put(parts[0], Boolean.parseBoolean(parts[1]));
+        }
+
+        if (commandMap.get("W")) {
+            newPosition = new Point(currentPosition.x, currentPosition.y + 1);
+        } else if (commandMap.get("S")) {
+            newPosition = new Point(currentPosition.x, currentPosition.y - 1);
+        } else if (commandMap.get("A")) {
+            newPosition = new Point(currentPosition.x - 1, currentPosition.y);
+        } else if (commandMap.get("D")) {
+            newPosition = new Point(currentPosition.x + 1, currentPosition.y);
         }
 
         if (newPosition != null) {
