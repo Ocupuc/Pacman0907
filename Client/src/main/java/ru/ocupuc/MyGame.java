d6f0ef3ad0e65339b0e0b7338ca7dfceab5b95ff
Ocupuc <108402919+Ocupuc@users.noreply.github.com>
2023-07-31 12:00:12 +0500

    Анимация

--- Client/src/main/java/ru/ocupuc/MyGame.java
+++ Client/src/main/java/ru/ocupuc/MyGame.java
@@ -3,7 +3,9 @@ package ru.ocupuc;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -13,43 +15,45 @@ import java.util.List;
 import static ru.ocupuc.GameData.*;
 
 public class MyGame extends ApplicationAdapter {
+    private static final int CELL_SIZE = 30;
+    private static final float FRAME_DURATION = 0.1f;
 
+    private Texture pacmanTextureSheet;
+    private Animation<TextureRegion> pacmanAnimation;
+    private float elapsedTime = 0;
 
-    Network network = new Network((args) -> { });
-    KeyboardTracker keyboardTracker = new KeyboardTracker(network);
-
-    private static final int CELL_SIZE = 30;  // размер клетки
-
-
-    private List<Texture> pacmanTextures = new ArrayList<Texture>();
-
-    private Texture pacmanTexture;
     private Texture wallTexture;
     private Texture pillTexture;
+
+    Network network = new Network((args) -> { });
+
+    KeyboardTracker keyboardTracker = new KeyboardTracker(network);
 
 
 
     @Override
     public void create() {
-
         Gdx.input.setInputProcessor(keyboardTracker);
-        pacmanTexture = new Texture(Gdx.files.internal("pacman.png"));
+
+        pacmanTextureSheet = new Texture(Gdx.files.internal("pacman1.png"));
+        TextureRegion[][] pacmanFrames = TextureRegion.split(pacmanTextureSheet, 50, 50);
+        pacmanAnimation = new Animation<TextureRegion>(FRAME_DURATION, pacmanFrames[0][0], pacmanFrames[1][1], pacmanFrames[2][0]);
+        pacmanAnimation.setPlayMode(Animation.PlayMode.LOOP);
+
         wallTexture = new Texture(Gdx.files.internal("wall.png"));
         pillTexture = new Texture(Gdx.files.internal("pill.png"));
-
-
-
     }
 
     @Override
     public void render() {
-
+        elapsedTime += Gdx.graphics.getDeltaTime();
         ScreenUtils.clear(0f, 0f, 0.1f, 1.0f);
         SpriteBatch batch = new SpriteBatch();
         batch.begin();
 
         for (Pacman pacman : enemyPacmans) {
-            batch.draw(pacmanTexture, pacman.getX() * CELL_SIZE, pacman.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            TextureRegion currentFrame = pacmanAnimation.getKeyFrame(elapsedTime);
+            batch.draw(currentFrame, pacman.getX() * CELL_SIZE, pacman.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
             pacman.animate();
         }
 
@@ -62,17 +66,16 @@ public class MyGame extends ApplicationAdapter {
         }
 
         if (myPacman != null) {
-            batch.draw(pacmanTexture, myPacman.getX() * CELL_SIZE, myPacman.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            TextureRegion currentFrame = pacmanAnimation.getKeyFrame(elapsedTime);
+            batch.draw(currentFrame, myPacman.getX() * CELL_SIZE, myPacman.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
+
         batch.end();
     }
 
-
-
-
     @Override
     public void dispose() {
-        pacmanTexture.dispose();
+        pacmanTextureSheet.dispose();
         wallTexture.dispose();
         pillTexture.dispose();
     }
